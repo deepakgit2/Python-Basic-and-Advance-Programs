d45d6bc66e3e8b78f6a5615ee8c34a1e0ecb3b5d
Deepak Singh <singh.deepak3096@gmail.com>
2019-04-09 12:00:59 +0530

    Update Tower_of_Hanoi_Animated.py

--- Tower_of_Hanoi_Animated.py
+++ Tower_of_Hanoi_Animated.py
@@ -1,22 +1,24 @@
-# It is the implementation of Towe of Henoi problem
+# It is the implementation of Towe of Henoi Problem
 # You can see animation when you run into terminal
+# Implemented --python-version 3.7
 
 import os, time
 
 n = 5
+clear = 'cls' if os.sys.platform == 'win32' else 'clear'
 # Initializing towers
-A,B,C = range(1,n+1),[],[]
+A, B, C = list(range(1,n+1)), [], []
 
 # To see all three tower
 def show(arr, frm, to):
 	gap = 20
-	print ' '*gap,'A   B   C'
+	print (' '*gap,'A  B  C')
 	for c in range(n):
-		print ' '*gap,
+		print (' '*gap, end=' ')
 		for r in range(3):
-			print arr[r][c],' ',
-		print '\n'
-	print 'Move a disk from tower {} to {}'.format(frm, to)
+			print (arr[r][c],' ', end='')
+		print ('\n')
+	print ( 'Move a disk from tower {} to {}'.format(frm, to) )
 	time.sleep(.5)
 
 # Move one disk from one tower to another tower
@@ -26,11 +28,11 @@ def move(frm, to):
 	exec('{}.insert(0,t)'.format(to))
 
 	arr = [[0]*(n-len(A))+A, [0]*(n-len(B))+B, [0]*(n-len(C))+C]
-	os.system('clear')
+	os.system(clear)
 	show(arr, frm, to)
 	
 # Recursive Function
-def TOH(n,frm,to,empty):
+def TOH(n, frm, to, empty):
 	if n == 1:
 		move(frm, to)
 	else:
@@ -38,4 +40,4 @@ def TOH(n,frm,to,empty):
 		TOH(1, frm, to, empty)
 		TOH(n-1, empty, to, frm)	
 
-TOH(n,'A','B','C')
+TOH(n, 'A', 'B', 'C')
